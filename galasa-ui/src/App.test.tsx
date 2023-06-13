4acb5ee2e3f3e10946e15bed10874ca4b6aa750e
Savvas Kyriacou <savvasky@uk.ibm.com>
2023-06-13 16:17:13 +0100

    code improvements
    Signed-off-by: Savvas Kyriacou <savvasky@uk.ibm.com>

--- galasa-ui/src/App.test.tsx
+++ galasa-ui/src/App.test.tsx
@@ -8,7 +8,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Galasa/i);
+  const linkElement = screen.getByText(/Galasa Ecosystem/i);
   expect(linkElement).toBeInTheDocument();
 });
 
