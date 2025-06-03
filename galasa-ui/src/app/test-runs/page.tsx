249521d13ca4d7fcc81d5f27e997c7645c2c5345
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-06-03 22:07:54 +0300

    feat: Add a test runs menu to the top bar and make it navigate to a new page
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

---
+++ galasa-ui/src/app/test-runs/page.tsx
@@ -0,0 +1,18 @@
+/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+import PageTile from "@/components/PageTile";
+import BreadCrumb from "@/components/common/BreadCrumb";
+
+
+export default function TestRunsPage() {
+  return (
+    <main id="content">
+      <BreadCrumb />
+      <PageTile title={"Test Runs"} />
+    </main>
+  );
+};
