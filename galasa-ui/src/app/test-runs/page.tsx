9151cbde48ecb853007b91264c3fa95d16627435
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-06-03 22:32:58 +0300

    feat: Add Test Runs Content component with placeholder content
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- galasa-ui/src/app/test-runs/page.tsx
+++ galasa-ui/src/app/test-runs/page.tsx
@@ -6,13 +6,14 @@
 
 import PageTile from "@/components/PageTile";
 import BreadCrumb from "@/components/common/BreadCrumb";
-
+import TestRunsContent from "@/components/test-runs/TestRunsContent";
 
 export default function TestRunsPage() {
   return (
     <main id="content">
       <BreadCrumb />
       <PageTile title={"Test Runs"} />
+      <TestRunsContent />
     </main>
   );
 };
