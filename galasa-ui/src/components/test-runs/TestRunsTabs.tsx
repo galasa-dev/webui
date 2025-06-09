f5eecefc532a5166e5df9ae4cba546c75873c14e
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-06-09 23:13:48 +0300

    refactor(test runs): adjust a hook to control the selected index
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- galasa-ui/src/components/test-runs/TestRunsTabs.tsx
+++ galasa-ui/src/components/test-runs/TestRunsTabs.tsx
@@ -8,6 +8,7 @@
  
 import { Tabs, Tab, TabList, TabPanels, TabPanel } from '@carbon/react'; 
 import styles from '@/styles/TestRunsPage.module.css';
+import { useState } from 'react';
 
 type TabLabel = 'Timeframe' | 'Table Design' | 'Search Criteria' | 'Results';
 interface TabConfig {
@@ -16,33 +17,34 @@ interface TabConfig {
 }
 
 // Currently, the content for each tab is static and under construction.
-const TimeFrameContent = () => <p className={styles.tabContent}>
+const TimeframeContent = () => <p>
     This page is under construction. Currently, all results for the last 24 hours are shown in the Results tab.
 </p>;
 
-const TableDesignContent = () => <p className={styles.tabContent}>
+const TableDesignContent = () => <p>
     This page is under construction. In future, you will be able to choose which columns are visible and their order.
 </p>;
 
-const SearchCriteriaContent = () => <p className={styles.tabContent}>
+const SearchCriteriaContent = () => <p>
     This page is under construction. Define specific search criteria to filter the results below.
 </p>;
 
-const ResultsContent = () => <p className={styles.tabContent}>
+const ResultsContent = () => <p>
     Results will be displayed here based on the selected timeframe and search criteria.
 </p>;
 
 // Define the tabs with their corresponding content.
 const TABS_CONFIG: TabConfig[] = [
-  {label: 'Timeframe', component: <TimeFrameContent />},
+  {label: 'Timeframe', component: <TimeframeContent />},
   {label: 'Table Design', component: <TableDesignContent />},
   {label: 'Search Criteria', component: <SearchCriteriaContent />},
   {label: 'Results', component: <ResultsContent />},
 ];
 
 export default function TestRunsTabs() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
-    <Tabs>
+    <Tabs selectedIndex={selectedIndex} onSelectionChange={(index: number) => setSelectedIndex(index)}>
       <TabList scrollDebounceWait={200}>
         {TABS_CONFIG.map((tab) => (
           <Tab key={tab.label}>{tab.label}</Tab>
@@ -51,7 +53,9 @@ export default function TestRunsTabs() {
       <TabPanels>
         {TABS_CONFIG.map((tab) => (
           <TabPanel key={tab.label}>
-            {tab.component}
+            <div className={styles.tabContent}>
+                {tab.component}
+            </div>
           </TabPanel>
         ))}
       </TabPanels>
