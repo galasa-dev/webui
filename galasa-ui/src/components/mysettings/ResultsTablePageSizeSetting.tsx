1b77675ccd9d6df4f4b7f50049b2424172c7f573
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-08-12 19:04:19 +0300

    feat(mysettings): hide results table page size setting when test run feature isn't enabled
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- galasa-ui/src/components/mysettings/ResultsTablePageSizeSetting.tsx
+++ galasa-ui/src/components/mysettings/ResultsTablePageSizeSetting.tsx
@@ -10,28 +10,35 @@ import { Dropdown } from '@carbon/react';
 import { useTranslations } from 'next-intl';
 import styles from '@/styles/mysettings/ResultsTablePageSizingSetting.module.css';
 import useResultsTablePageSize from '@/hooks/useResultsTablePageSize';
+import { useFeatureFlags } from '@/contexts/FeatureFlagContext';
+import { FEATURE_FLAGS } from '@/utils/featureFlags';
 
 export default function ResultsTablePageSizingSetting() {
   const translations = useTranslations('ResultsTablePageSizingSetting');
+  const { isFeatureEnabled } = useFeatureFlags();
   const { defaultPageSize, setDefaultPageSize } = useResultsTablePageSize();
 
   return (
-    <section className={styles.section}>
-      <h3 className={styles.heading}>{translations('title')}</h3>
-      <div>
-        <p className={styles.title}>{translations('description')}</p>
-        <div className={styles.dropdownContainer}>
-          <p>{translations('defaultTestRunsLabel')}</p>
-          <Dropdown
-            data-testid="custom-items-per-page-dropdown-test"
-            items={RESULTS_TABLE_PAGE_SIZES}
-            itemToString={(item: number) => item.toString()}
-            selectedItem={defaultPageSize}
-            onChange={(e: { selectedItem: number }) => setDefaultPageSize(e.selectedItem)}
-            size="md"
-          />
-        </div>
-      </div>
-    </section>
+    <>
+      {isFeatureEnabled(FEATURE_FLAGS.TEST_RUNS) && (
+        <section className={styles.section}>
+          <h3 className={styles.heading}>{translations('title')}</h3>
+          <div>
+            <p className={styles.title}>{translations('description')}</p>
+            <div className={styles.dropdownContainer}>
+              <p>{translations('defaultTestRunsLabel')}</p>
+              <Dropdown
+                data-testid="custom-items-per-page-dropdown-test"
+                items={RESULTS_TABLE_PAGE_SIZES}
+                itemToString={(item: number) => item.toString()}
+                selectedItem={defaultPageSize}
+                onChange={(e: { selectedItem: number }) => setDefaultPageSize(e.selectedItem)}
+                size="md"
+              />
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
