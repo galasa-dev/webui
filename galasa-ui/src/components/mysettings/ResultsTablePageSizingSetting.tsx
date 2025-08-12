168fd64cb21ad8ce5929da770ae9203a78e83074
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-08-12 14:57:09 +0300

    feat(mysettings): style the added section
    Signed-off-by: Tasneemmhammed0 <tasneemmohammed098@gmail.com>

--- galasa-ui/src/components/mysettings/ResultsTablePageSizingSetting.tsx
+++ galasa-ui/src/components/mysettings/ResultsTablePageSizingSetting.tsx
@@ -8,15 +8,16 @@
 import { RESULTS_TABLE_PAGE_SIZES } from '@/utils/constants/common';
 import { Dropdown } from '@carbon/react';
 import { useTranslations } from 'next-intl';
+import styles from '@/styles/mysettings/ResultsTablePageSizingSetting.module.css';
 
 export default function ResultsTablePageSizingSetting() {
   const translations = useTranslations('ResultsTablePageSizingSetting');
   return (
-    <section>
-      <h3>{translations('title')}</h3>
+    <section className={styles.section}>
+      <h3 className={styles.heading}>{translations('title')}</h3>
       <div>
-        <p>{translations('description')}</p>
-        <div>
+        <p className={styles.title}>{translations('description')}</p>
+        <div className={styles.dropdownContainer}>
           <p>{translations('defaultTestRunsLabel')}</p>
           <Dropdown
             data-testid="custom-items-per-page-dropdown-test"
