e7c9c91be478a4a0f9307ad77090559e3390aff3
James Cocker <james.s.earth@gmail.com>
2025-10-17 10:23:17 +0100

    Fixed unapplied CSS on the screenshot skeleton
    Signed-off-by: James Cocker <james.s.earth@gmail.com>

--- galasa-ui/src/components/test-runs/test-run-details/3270Tab/TableOfScreenshots.tsx
+++ galasa-ui/src/components/test-runs/test-run-details/3270Tab/TableOfScreenshots.tsx
@@ -26,7 +26,7 @@ import { DataTableHeader, DataTableRow } from '@/utils/interfaces';
 import { get3270Screenshots } from '@/utils/3270/get3270Screenshots';
 import { useTranslations } from 'next-intl';
 import { TreeNodeData } from '@/utils/functions/artifacts';
-import styles from '@/styles/test-runs/test-run-details/tab3270.module.css';
+import styles from '@/styles/test-runs/test-run-details/Tab3270.module.css';
 import { CellFor3270, TerminalImage, DropdownOption } from '@/utils/interfaces/3270Terminal';
 
 export default function TableOfScreenshots({
