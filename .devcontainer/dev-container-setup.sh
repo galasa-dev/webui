4e0c853d40855ae8c457e19323be418bf254fbc2
James Cocker <james.s.earth@gmail.com>
2025-11-06 15:48:29 +0000

    Separated out python package install to dev container setup
    Signed-off-by: James Cocker <james.s.earth@gmail.com>

--- .devcontainer/dev-container-setup.sh
+++ .devcontainer/dev-container-setup.sh
@@ -6,20 +6,36 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
-GALASA_DEV_TOKEN_VALUE="$1"
-GALASA_API_SERVER_URL_VALUE="$2"
-SOURCE_MAVEN_VALUE="$3"
+function set_env_vars() {
+  local GALASA_DEV_TOKEN_VALUE="$1"
+  local GALASA_API_SERVER_URL_VALUE="$2"
+  local SOURCE_MAVEN_VALUE="$3"
+  local PROFILE_FILE="/home/node/.bashrc"
 
-PROFILE_FILE="/home/node/.bashrc"
+  if [ -n "$GALASA_DEV_TOKEN_VALUE" ]; then
+    echo "export GALASA_DEV_TOKEN=$GALASA_DEV_TOKEN_VALUE" >> "$PROFILE_FILE"
+  fi
 
-if [ -n "$GALASA_DEV_TOKEN_VALUE" ]; then
-  echo "export GALASA_DEV_TOKEN=$GALASA_DEV_TOKEN_VALUE" >> $PROFILE_FILE
-fi
+  if [ -n "$GALASA_API_SERVER_URL_VALUE" ]; then
+    echo "export GALASA_API_SERVER_URL=$GALASA_API_SERVER_URL_VALUE" >> "$PROFILE_FILE"
+  fi
 
-if [ -n "$GALASA_API_SERVER_URL_VALUE" ]; then
-  echo "export GALASA_API_SERVER_URL=$GALASA_API_SERVER_URL_VALUE" >> $PROFILE_FILE
-fi
+  if [ -n "$SOURCE_MAVEN_VALUE" ]; then
+    echo "export SOURCE_MAVEN=$SOURCE_MAVEN_VALUE" >> "$PROFILE_FILE"
+  fi
+}
 
-if [ -n "$SOURCE_MAVEN_VALUE" ]; then
-  echo "export SOURCE_MAVEN=$SOURCE_MAVEN_VALUE" >> $PROFILE_FILE
-fi
+function setup_python {
+  which pip3
+  rc=$?
+  if [[ "${rc}" != "0" ]]; then
+    error "pip3 tool is not available on your path. Install pip3 and re-try to install required python packages."
+    exit 1
+  fi
+  success "pip3 is available. OK"
+
+  pip3 install pre-commit
+}
+
+set_env_vars "$1" "$2" "$3"
+setup_python
