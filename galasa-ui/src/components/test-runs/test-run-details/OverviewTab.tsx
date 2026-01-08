852d4fd41d0e8591a47ae06cb89e7a2125850d50
James Cocker <james.s.earth@gmail.com>
2026-01-08 12:06:59 +0000

    Button and modal added
    Signed-off-by: James Cocker <james.s.earth@gmail.com>

--- galasa-ui/src/components/test-runs/test-run-details/OverviewTab.tsx
+++ galasa-ui/src/components/test-runs/test-run-details/OverviewTab.tsx
@@ -11,10 +11,13 @@ import { Tag } from '@carbon/react';
 import { RunMetadata } from '@/utils/interfaces';
 import { useTranslations } from 'next-intl';
 import { Link } from '@carbon/react';
-import { Launch } from '@carbon/icons-react';
+import { Launch, Edit } from '@carbon/icons-react';
 import { getAWeekBeforeSubmittedTime } from '@/utils/timeOperations';
 import useHistoryBreadCrumbs from '@/hooks/useHistoryBreadCrumbs';
 import { TEST_RUNS_QUERY_PARAMS } from '@/utils/constants/common';
+import { TextInput } from '@carbon/react';
+import { Modal } from '@carbon/react';
+import { DismissibleTag } from '@carbon/react';
 
 const OverviewTab = ({ metadata }: { metadata: RunMetadata }) => {
   const tags = metadata?.tags || [];
@@ -22,6 +25,8 @@ const OverviewTab = ({ metadata }: { metadata: RunMetadata }) => {
   const { pushBreadCrumb } = useHistoryBreadCrumbs();
 
   const [weekBefore, setWeekBefore] = useState<string | null>(null);
+
+  const [isTagsEditModalOpen, setIsTagsEditModalOpen] = useState<Boolean>(false);
 
   const fullTestName = metadata?.testName;
   const OTHER_RECENT_RUNS = `/test-runs?${TEST_RUNS_QUERY_PARAMS.TEST_NAME}=${fullTestName}&${TEST_RUNS_QUERY_PARAMS.BUNDLE}=${metadata?.bundle}&${TEST_RUNS_QUERY_PARAMS.PACKAGE}=${metadata?.package}&${TEST_RUNS_QUERY_PARAMS.DURATION}=60,0,0&${TEST_RUNS_QUERY_PARAMS.TAB}=results&${TEST_RUNS_QUERY_PARAMS.QUERY_NAME}=Recent runs of test ${metadata?.testName}`;
@@ -64,7 +69,13 @@ const OverviewTab = ({ metadata }: { metadata: RunMetadata }) => {
       </div>
 
       <div className={styles.tagsSection}>
-        <h5>{translations('tags')}</h5>
+        <h5>
+          {translations('tags')}
+
+          <div className={styles.tagsEditWrapper} onClick={() => setIsTagsEditModalOpen(true)}>
+            <Edit className={styles.tagsEditButton}/>
+          </div>
+        </h5>
         <div className={styles.tagsContainer}>
           {tags?.length > 0 ? (
             tags?.map((tag, index) => (
@@ -93,6 +104,43 @@ const OverviewTab = ({ metadata }: { metadata: RunMetadata }) => {
           )}
         </div>
       </div>
+
+      {isTagsEditModalOpen && (
+        <Modal
+          open={true}
+          onRequestClose={() => setIsTagsEditModalOpen(false)}
+          modalHeading={`${translations('modalHeading')} ${metadata.runName}`}
+          primaryButtonText={translations('modalPrimaryButton')}
+          secondaryButtonText={translations('modalSecondaryButton')}
+        >
+          <TextInput
+            data-modal-primary-focus
+            id="text-input-1"
+            labelText={translations('modalLabelText')}
+            placeholder={translations('modalPlaceholderText')}
+            style={{
+              marginBottom: '24px'
+            }}
+          />
+          <div className={styles.tagsContainer}>
+          {tags?.length > 0 ? (
+            tags?.map((tag, index) => (
+              <DismissibleTag
+                key={index}
+                dismissTooltipAlignment="bottom"
+                onClose={()=> {}}
+                size="md"
+                text={tag}
+                title={translations('removeTag')}
+                // type="red"
+              />
+            ))
+          ) : (
+            <p>{translations('noTags')}</p>
+          )}
+        </div>
+        </Modal>
+      )}
     </>
   );
 };
