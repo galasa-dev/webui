274205f59dd3e0c67f34135e9b09161e84a392a4
James Cocker <james.s.earth@gmail.com>
2026-03-30 12:16:49 +0100

    Removed custom serialiser
    Signed-off-by: James Cocker <james.s.earth@gmail.com>

--- galasa-ui/setupTests.ts
+++ galasa-ui/setupTests.ts
@@ -9,49 +9,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import 'isomorphic-fetch';
-// Add snapshot serializer for readable React component snapshots
-// This converts React elements to a readable JSX-like format instead of verbose object notation
-// Compatible with React 19 and Next.js 16
-expect.addSnapshotSerializer({
-  test: (val) => val && val.$$typeof === Symbol.for('react.transitional.element'),
-  serialize: (val: any, config, indentation, depth, refs, printer) => {
-    // Convert React element to a more readable format
-    const { type, props } = val;
-    const typeName = typeof type === 'function' ? type.name || 'Component' : type;
-
-    // Build a JSX-like representation
-    let result = `<${typeName}`;
-
-    // Add props
-    if (props) {
-      const { children, ...otherProps } = props;
-      const propKeys = Object.keys(otherProps);
-
-      if (propKeys.length > 0) {
-        result += '\n';
-        propKeys.forEach((key) => {
-          const value = otherProps[key];
-          const valueStr = printer(value, config, indentation + config.indent, depth, refs);
-          result += `${indentation}${config.indent}${key}=${valueStr}\n`;
-        });
-        result += indentation;
-      }
-
-      if (children !== undefined && children !== null) {
-        result += '>\n';
-        const childrenStr = printer(children, config, indentation + config.indent, depth, refs);
-        result += `${indentation}${config.indent}${childrenStr}\n`;
-        result += `${indentation}</${typeName}>`;
-      } else {
-        result += ' />';
-      }
-    } else {
-      result += ' />';
-    }
-
-    return result;
-  },
-});
 
 // This is needed for Carbon Design System components that use scrollIntoView
 if (typeof Element !== 'undefined') {
