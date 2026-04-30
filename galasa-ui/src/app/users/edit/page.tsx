3bcac21845dd5aa1a50fcc98c53f851731727594
James Cocker <james.s.earth@gmail.com>
2026-04-30 10:58:24 +0100

    Fix build process
    Signed-off-by: James Cocker <james.s.earth@gmail.com>

--- galasa-ui/src/app/users/edit/page.tsx
+++ galasa-ui/src/app/users/edit/page.tsx
@@ -5,18 +5,42 @@
  */
 import PageTile from '@/components/PageTile';
 import UserRoleSection from '@/components/users/UserRoleSection';
-import { RBACRole, RoleBasedAccessControlAPIApi } from '@/generated/galasaapi';
+import { ConfigurationPropertyStoreAPIApi, RBACRole, RoleBasedAccessControlAPIApi } from '@/generated/galasaapi';
 import { createAuthenticatedApiConfiguration } from '@/utils/api';
 import AccessTokensSection from '@/components/mysettings/AccessTokensSection';
 import { fetchAccessTokens } from '@/actions/getUserAccessTokens';
 import { fetchUserFromApiServer } from '@/actions/userServerActions';
 import BreadCrumb from '@/components/common/BreadCrumb';
 import { EDIT_USER, HOME } from '@/utils/constants/breadcrumb';
+import * as Constants from '@/utils/constants/common';
 
 // In order to extract query param on server-side
 type UsersPageProps = {
   params: Promise<{}>; // No dynamic route parameters
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+const getValidatedWarningDays = (value?: string) => {
+  const parsedValue = Number.parseInt(value ?? '', 10);
+
+  if (Number.isNaN(parsedValue) || parsedValue < 0) {
+    return {
+      warningDays: Constants.DEFAULT_ACCESS_TOKEN_EXPIRY_WARNING_DAYS,
+      exceededMaximum: false,
+    };
+  }
+
+  if (parsedValue > Constants.MAX_ACCESS_TOKEN_EXPIRY_WARNING_DAYS) {
+    return {
+      warningDays: Constants.MAX_ACCESS_TOKEN_EXPIRY_WARNING_DAYS,
+      exceededMaximum: true,
+    };
+  }
+
+  return {
+    warningDays: parsedValue,
+    exceededMaximum: false,
+  };
 };
 
 export default async function EditUserPage(props: UsersPageProps) {
@@ -37,6 +61,24 @@ export default async function EditUserPage(props: UsersPageProps) {
 
     return roles;
   };
+
+  const fetchTokenExpiryWarningConfiguration = async () => {
+    try {
+      const cpsApiClientWithAuthHeader = new ConfigurationPropertyStoreAPIApi(apiConfig);
+      const warningPropertyResponse = await cpsApiClientWithAuthHeader.getCpsProperty(
+        Constants.ACCESS_TOKEN_EXPIRY_WARNING_PROPERTY_NAMESPACE,
+        Constants.ACCESS_TOKEN_EXPIRY_WARNING_PROPERTY_NAME
+      );
+
+      const warningPropertyValue = warningPropertyResponse?.[0]?.data?.value;
+      return getValidatedWarningDays(warningPropertyValue);
+    } catch (error) {
+      return getValidatedWarningDays();
+    }
+  };
+
+  const tokenExpiryWarningConfiguration = await fetchTokenExpiryWarningConfiguration();
+
   return (
     <main id="content">
       <BreadCrumb breadCrumbItems={[HOME, EDIT_USER]} />
@@ -48,6 +90,8 @@ export default async function EditUserPage(props: UsersPageProps) {
       <AccessTokensSection
         accessTokensPromise={fetchAccessTokens(loginIdFromQueryParam)}
         isAddBtnVisible={false}
+        tokenExpiryWarningDays={tokenExpiryWarningConfiguration.warningDays}
+        showMaxWarningDaysNotice={tokenExpiryWarningConfiguration.exceededMaximum}
       />
     </main>
   );
